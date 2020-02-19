a7b6e6cb9338bede88f0f4931f56de0903e14d19
Ian Dietsch <wolffox36007@gmail.com>
2020-02-19 15:22:14 -0500

    Add project files.

---
+++ ExceptionsLearning/BootcampException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsLearning {
+    public class BootcampException : Exception{
+
+        public BootcampException() : base() { }
+
+        public BootcampException(string message) : base(message) {
+
+        }
+
+        public BootcampException(string message, Exception innerException) :base (message, innerException) {
+
+        }
+    }
+}
---
+++ ExceptionsLearning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExceptionsLearning {
+    class Program {
+        static void Main(string[] args) {
+            var a = 1;
+            var b = 2;
+            try {
+
+                string x = "ABC";
+                x = null;
+                var len = x.Length;
+            } catch (NullReferenceException ex) {
+                var bce = new BootcampException("Bootcamp Exception", ex);
+                throw bce;
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            try {
+            var c = a / (b - b);
+
+            } catch (DivideByZeroException ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            //var d = 1 / 0; //divide by 0 causes error
+        }
+    }
+}
